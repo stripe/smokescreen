edb6b3fff58d101f76638e355d0a62387ef642f8
Ryan Koppenhaver <rlk@stripe.com>
2018-09-28 10:02:54 -0700

    add role tests

---
+++ pkg/smokescreen/role_test.go
@@ -0,0 +1,47 @@
+package smokescreen
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func mockRFR(s string, e error) func(req *http.Request) (string, error) {
+	return func(req *http.Request) (string, error) {
+		return s, e
+	}
+}
+
+func _testGetRole(t *testing.T, rfr_s string, rfr_e error, allow_missing bool, expect_s string, expect_e error) {
+	config := Config{
+		RoleFromRequest:  mockRFR(rfr_s, rfr_e),
+		AllowMissingRole: allow_missing,
+	}
+	s, e := getRole(&config, nil)
+	if (e == nil) != (expect_e == nil) {
+		t.Fatalf("expected err %v got %v\n", expect_e, e)
+	}
+	if s != expect_s {
+		t.Fatalf("expected role %v got %v\n", expect_s, s)
+	}
+}
+
+func TestGetRole(t *testing.T) {
+	role := "some role"
+	genErr := errors.New("general error")
+	mre := MissingRoleError("missing role")
+
+	t.Run("good", func(t *testing.T) {
+		_testGetRole(t, role, nil, false, role, nil)
+	})
+	t.Run("bad", func(t *testing.T) {
+		_testGetRole(t, "", genErr, false, "", genErr)
+	})
+
+	t.Run("missing not allowed", func(t *testing.T) {
+		_testGetRole(t, "", mre, false, "", genErr)
+	})
+	t.Run("missing allowed", func(t *testing.T) {
+		_testGetRole(t, "", mre, true, "", nil)
+	})
+}
