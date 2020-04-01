1b2325d4b55ea5f47e495abf32c9bbb085bfb9ed
Craig Shannon <cds@stripe.com>
2020-03-31 20:31:37 -0700

    make net.Error more granular

--- pkg/smokescreen/smokescreen_test.go
+++ pkg/smokescreen/smokescreen_test.go
@@ -522,7 +522,7 @@ func TestProxyTimeouts(t *testing.T) {
 		r.NoError(err)
 
 		resp, _ := client.Do(req)
-		r.Equal(http.StatusBadGateway, resp.StatusCode)
+		r.Equal(http.StatusGatewayTimeout, resp.StatusCode)
 		r.NotEqual("", resp.Header.Get(errorHeader))
 
 		entry := findCanonicalProxyDecision(logHook.AllEntries())
@@ -582,7 +582,7 @@ func TestProxyTimeouts(t *testing.T) {
 		resp, err := client.Do(req)
 		r.Nil(resp)
 		r.Error(err)
-		r.Contains(err.Error(), "Bad gateway")
+		r.Contains(err.Error(), "Gateway timeout")
 	})
 
 	t.Run("HTTP proxy dial timeouts", func(t *testing.T) {
@@ -606,7 +606,7 @@ func TestProxyTimeouts(t *testing.T) {
 		r.NoError(err)
 
 		resp, _ := client.Do(req)
-		r.Equal(http.StatusBadGateway, resp.StatusCode)
+		r.Equal(http.StatusGatewayTimeout, resp.StatusCode)
 		r.NotEqual("", resp.Header.Get(errorHeader))
 	})
 }
