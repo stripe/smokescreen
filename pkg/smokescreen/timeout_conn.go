d21ef0b5a88891f9be9e0e15c2d55d1390cbe0d5
Ben Ransford <ransford@stripe.com>
2020-05-13 22:48:20 -0700

    rearrange SetDeadline w/r/t Read/Write, per previous commit

--- pkg/smokescreen/timeout_conn.go
+++ pkg/smokescreen/timeout_conn.go
@@ -18,21 +18,33 @@ func NewTimeoutConn(conn net.Conn, timeout time.Duration) net.Conn {
 }
 
 func (tc *TimeoutConn) Read(b []byte) (int, error) {
+	n, err := tc.Conn.Read(b)
+	if err != nil {
+		return n, err
+	}
+
 	if tc.timeout != 0 {
 		err := tc.Conn.SetDeadline(time.Now().Add(tc.timeout))
 		if err != nil {
 			return 0, err
 		}
 	}
-	return tc.Conn.Read(b)
+
+	return n, nil
 }
 
 func (tc *TimeoutConn) Write(b []byte) (int, error) {
+	n, err := tc.Conn.Write(b)
+	if err != nil {
+		return n, err
+	}
+
 	if tc.timeout != 0 {
 		err := tc.Conn.SetDeadline(time.Now().Add(tc.timeout))
 		if err != nil {
 			return 0, err
 		}
 	}
-	return tc.Conn.Write(b)
+
+	return n, nil
 }
