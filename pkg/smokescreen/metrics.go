1c68f97c9662d3658002891dadad1658970fc1d6
Craig Shannon <cds@stripe.com>
2021-04-15 14:44:20 -0500

    pr feedback

--- pkg/smokescreen/metrics.go
+++ pkg/smokescreen/metrics.go
@@ -7,31 +7,37 @@ import (
 	"github.com/DataDog/datadog-go/statsd"
 )
 
-var metrics = map[string][]string{
-	"acl.allow":                        {},
-	"acl.decide_error":                 {},
-	"acl.deny":                         {},
-	"acl.report":                       {},
-	"acl.role_not_determined":          {},
-	"acl.unknown_error":                {},
-	"cn.atpt.connect.time":             {},
-	"cn.atpt.fail.total":               {},
-	"cn.atpt.success.total":            {},
-	"cn.atpt.total":                    {},
-	"resolver.allow.default":           {},
-	"resolver.allow.user_configured":   {},
-	"resolver.attempts_total":          {},
-	"resolver.deny.not_global_unicast": {},
-	"resolver.deny.private_range":      {},
-	"resolver.deny.user_configured":    {},
-	"resolver.errors_total":            {},
+// metrics contains all of the metric names contained within the smokescreen package.
+// These are used to determine if a given metric name is valid before associating
+// a persistent tag with the metric. This list must be updated with new metric names
+// if the metric should support persistent tagging.
+var metrics = []string{
+	"acl.allow",
+	"acl.decide_error",
+	"acl.deny",
+	"acl.report",
+	"acl.role_not_determined",
+	"acl.unknown_error",
+	"cn.atpt.connect.time",
+	"cn.atpt.fail.total",
+	"cn.atpt.success.total",
+	"cn.atpt.total",
+	"resolver.allow.default",
+	"resolver.allow.user_configured",
+	"resolver.attempts_total",
+	"resolver.deny.not_global_unicast",
+	"resolver.deny.private_range",
+	"resolver.deny.user_configured",
+	"resolver.errors_total",
 }
 
 // MetricsClient is a thin wrapper around statsd.ClientInterface. It is used to allow
 // adding arbitrary tags to Smokescreen metrics.
+//
+// MetricsClient is not thread safe and should not be used concurrently.
 type MetricsClient struct {
-	additionalTags map[string][]string
-	StatsdClient   statsd.ClientInterface
+	metricsTags  map[string][]string
+	StatsdClient statsd.ClientInterface
 }
 
 // NewMetricsClient creates a new MetricsClient with the provided statsd address and
@@ -43,9 +49,15 @@ func NewMetricsClient(addr, namespace string) (*MetricsClient, error) {
 	}
 	c.Namespace = namespace
 
+	// Populate the client's map to hold metric tags
+	metricsTags := make(map[string][]string)
+	for _, m := range metrics {
+		metricsTags[m] = []string{}
+	}
+
 	return &MetricsClient{
-		additionalTags: make(map[string][]string),
-		StatsdClient:   c,
+		metricsTags:  metricsTags,
+		StatsdClient: c,
 	}, nil
 }
 
@@ -53,16 +65,19 @@ func NewMetricsClient(addr, namespace string) (*MetricsClient, error) {
 // be used when there's no statsd service available to smokescreen.
 func NewNoOpMetricsClient() *MetricsClient {
 	return &MetricsClient{
-		additionalTags: make(map[string][]string),
-		StatsdClient:   &statsd.NoOpClient{},
+		metricsTags:  make(map[string][]string),
+		StatsdClient: &statsd.NoOpClient{},
 	}
 }
 
 // AddMetricTags associates the provided tags slice with a given metric. The metric must be present
 // in the metrics slice.
+//
+// This function is not thread safe, and adding persitent tags should only be done while initializing
+// the configuration and prior to running smokescreen.
 func (mc *MetricsClient) AddMetricTags(metric string, mTags []string) error {
-	if tags, ok := metrics[metric]; ok {
-		metrics[metric] = append(tags, mTags...)
+	if tags, ok := mc.metricsTags[metric]; ok {
+		mc.metricsTags[metric] = append(tags, mTags...)
 		return nil
 	}
 	return fmt.Errorf("unknown metric: %s", metric)
@@ -70,7 +85,7 @@ func (mc *MetricsClient) AddMetricTags(metric string, mTags []string) error {
 
 // GetMetricTags returns the slice of metrics associated with a given metric.
 func (mc *MetricsClient) GetMetricTags(metric string) []string {
-	if tags, ok := metrics[metric]; ok {
+	if tags, ok := mc.metricsTags[metric]; ok {
 		return tags
 	}
 	return nil
@@ -95,5 +110,5 @@ func (mc *MetricsClient) Timing(metric string, d time.Duration, rate float64) er
 func (mc *MetricsClient) TimingWithTags(metric string, d time.Duration, rate float64, tags []string) error {
 	mTags := mc.GetMetricTags(metric)
 	tags = append(tags, mTags...)
-	return mc.StatsdClient.Timing(metric, d, mTags, rate)
+	return mc.StatsdClient.Timing(metric, d, tags, rate)
 }
