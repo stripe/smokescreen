3592fff8af08586cfe0773b60bae39f22532a701
lewandowski-stripe <108926515+lewandowski-stripe@users.noreply.github.com>
2022-07-22 15:33:52 +0200

    Improve address normalization (#164)
    * Be more strict with address and port normalization
    
    - Ensure that port is a number within a permissible range.
    - Ensure that IP addresses have a standardized representation.
    - Use net.LookupPort() to determine port number from scheme.
    
    * Parse the request address only once and simplify it
    
    - Refactor normalizeHost() so it returns host and port separately.
    - Replace regex-based host validation (hostExtractRE) with a simple
      string check. This has potential to remove some lock contention.
    
    * Use the same address for connecting and ACL checks
    
    This eliminates a potential mismatch between what was checked and what
    was used.
    
    * Normalize domain names with Punycode conversion
    
    * Create denyError{} outside of normalizeHost()
    
    * Add doc comment on normalizeHost()
    
    * Add support for bare IPv6 addresses (without port)
    
    * Add tests for normalizeHost()
    
    * Rename socketAddress to hostPort
    
    * Move port{Min,Max} to constants.go, define noPort
    
    * Add an forceFQDN option to normalizeHost
    
    * Be less verbose about errors from net.LookupPort
    
    * Improve comment in normalizeHost()
    
    * Wrap an error in denyError
    
    Co-authored-by: jjiang-stripe <55402658+jjiang-stripe@users.noreply.github.com>
    
    * Fix failing TestInvalidHost
    
    * Add a comment clarifying IP address normalization
    
    * Add more comments about IP address normalization
    
    * Fix the permissible port range check
    
    Co-authored-by: jjiang-stripe <55402658+jjiang-stripe@users.noreply.github.com>

--- pkg/smokescreen/constants.go
+++ pkg/smokescreen/constants.go
@@ -1,10 +1,7 @@
 package smokescreen
 
-import (
-	"regexp"
-)
-
 const versionSemantic = "0.0.3"
+const portMin, portMax = 0, 65535
 
 // This can be set at build time:
 // go build -ldflags='-X github.com/stripe/smokescreen/pkg/smokescreen.VersionID=33955a3' .
@@ -15,15 +12,3 @@ func Version() string {
 }
 
 const DefaultStatsdNamespace = "smokescreen."
-
-// Using a globally-shared Regexp can impact performace due to lock contention,
-// but calling Copy() for each connection is much worse, and it looks like
-// handing out Regexps from a pool doesn't save us anything either, so we'll
-// just live with it.
-const hostExtractPattern = "^([^:]*)(:\\d+)?$"
-
-var hostExtractRE *regexp.Regexp
-
-func init() {
-	hostExtractRE = regexp.MustCompile(hostExtractPattern)
-}
