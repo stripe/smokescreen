6f13b30d45f6cadc205179819ad3d31ab55d303b
cmoresco-stripe <106690468+cmoresco-stripe@users.noreply.github.com>
2023-07-26 13:25:03 -0400

    add AcceptResponseHandler to modify accepted responses (#196)
    * add AcceptResponseHandler to modify accepted responses
    
    * customer->custom

--- pkg/smokescreen/config.go
+++ pkg/smokescreen/config.go
@@ -79,8 +79,11 @@ type Config struct {
 	// Custom Dial Timeout function to be called
 	ProxyDialTimeout func(ctx context.Context, network, address string, timeout time.Duration) (net.Conn, error)
 
-	// Customer handler to allow clients to modify reject responses
+	// Custom handler to allow clients to modify reject responses
 	RejectResponseHandler func(*http.Response)
+
+	// Custom handler to allow clients to modify accept responses
+	AcceptResponseHandler func(*http.Response)
 
 	// UnsafeAllowPrivateRanges inverts the default behavior, telling smokescreen to allow private IP
 	// ranges by default (exempting loopback and unicast ranges)
